ef3473c2799fdc4d10712aecb8607ae256e269cf
Semenovsd <semenov_sd@bk.ru>
2020-10-09 14:48:25 +0300

    refact

--- app/handlers_main.py
+++ app/handlers_main.py
@@ -18,32 +18,58 @@ withdraw_cb = CallbackData('withdraw', 'action', 'club', 'method')
 
 @dp.callback_query_handler(text='start')
 @dp.message_handler(CommandStart())
-async def start_cmd_handler(message: Union[Message, CallbackQuery]):
-    '''
-    Стартовый экран при заходе в бота. Выводи список доступных клубов из джейсон файла.
+async def start_cmd_handler(message: Union[Message, CallbackQuery], state: FSMContext):
+    """
+    Стартовый экран приветствия.
     :param message:
     :return:
-    '''
+    """
+    await state.finish()
+    # todo
     # args = message.get_args() if hasattr(message, 'get_args') else None
     # зарегистрировать пользователя с данными из диплинка
+    keyboard_markup = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='Show clubs', callback_data='show_clubs')],
+            [InlineKeyboardButton(text='Contacts', callback_data='contacts')]
+        ]
+    )
+    await bot.send_sticker(message.from_user.id,
+                           sticker='CAACAgIAAxkBAAIEkl-ASlxpJp73d5zjlQZ_jq8juS2EAAJnAQACufOXC1vlyNMSEWLrGwQ')
+    reply = 'Hello! I can help you make withdraws or deposit! Press show clubs button.'
+    await bot.send_message(message.from_user.id, text=reply, reply_markup=keyboard_markup)
+
+
+@dp.callback_query_handler(text='show_clubs')
+async def choice_method(call: CallbackQuery, callback_data: Dict[str, str]):
+    """
+    Стартовый экран при заходе в бота. Выводи список доступных клубов из джейсон файла.
+    :param call:
+    :param callback_data:
+    :return:
+    """
+    # await call.answer()
+    await call.message.edit_reply_markup()
+
+    keyboard_markup = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='Contacts', callback_data='contacts')]]
+    )
 
     data = read_json_data(JSON_DATA_FILENAME)
     if data != 'File not read' and data.get('Clubs'):
         for club in data.get('Clubs'):
-            keyboard_markup = InlineKeyboardMarkup(
-                inline_keyboard=[
-                    [InlineKeyboardButton(text='Пополнить',
-                                          callback_data=deposit_cb.new(club=club.get('Club_name')))]]
+            keyboard_markup.add(
+                InlineKeyboardButton(text=f'{club.get("Club_name")}',
+                                     callback_data=deposit_cb.new(club=club.get('Club_name')))
             )
-            reply = f'Пополнить для клуба {club.get("Club_name")}'
-            await bot.send_message(message.from_user.id, text=reply, reply_markup=keyboard_markup)
+        reply = f'Choice the Club.'
+        await call.message.answer(reply, reply_markup=keyboard_markup)
     else:
-        keyboard_markup = InlineKeyboardMarkup(
-            inline_keyboard=[
-                [InlineKeyboardButton(text='Контакты для связи', callback_data='contacts')]]
-        )
-        reply = 'В данный момент сервис не доступен, попробуйте позднее.'
-        await bot.send_message(message.from_user.id, text=reply, reply_markup=keyboard_markup)
+        reply = 'The service is temporarily unavailable, please try again later'
+        await call.message.answer_sticker(
+            sticker='CAACAgIAAxkBAAIElF-ATNnW9y3WVWgDjLxOC5yvhY5QAAIUAQACufOXC5ZJByYQOH2bGwQ')
+        await call.message.answer(reply, reply_markup=keyboard_markup)
 
 
 @dp.callback_query_handler(deposit_cb.filter())
@@ -59,8 +85,10 @@ async def choice_method(call: CallbackQuery, callback_data: Dict[str, str]):
     # await call.message.edit_reply_markup()
     keyboard_markup = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text='Назад',
-                                  callback_data='start')]]
+            [InlineKeyboardButton(text='Back',
+                                  callback_data='start')],
+            [InlineKeyboardButton(text='Contacts', callback_data='contacts')]
+        ]
     )
     # в следующем окне отправляем уведомление в чат.
     data = read_json_data(JSON_DATA_FILENAME)
@@ -76,7 +104,7 @@ async def choice_method(call: CallbackQuery, callback_data: Dict[str, str]):
                                                                            method=value.get('Name'))),
                     )
 
-    reply = f'Варианты пополнения для клуба {current_club}'
+    reply = f'Choice available method for {current_club}'
     await call.message.answer(reply, reply_markup=keyboard_markup)
 
 
@@ -203,4 +231,3 @@ async def no_photo_prove(message: Message):
 
     reply = f'Я принимаю только ФОТО. Отправь фото или нажми отменя для возврата в предыдущее меню.'
     await message.answer(reply, reply_markup=keyboard_markup)
-
